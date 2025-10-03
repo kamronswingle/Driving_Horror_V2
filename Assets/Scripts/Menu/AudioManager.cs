3847bb9af3c6fabcd3760bfeadb4fecd6a92b60f
Kamron Swingle <kamswingle@gmail.com>
2025-10-02 20:23:00 -0700

    Added main menu background, and audio

---
+++ Assets/Scripts/Menu/AudioManager.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public struct MusicTrack
+{
+    public string trackName;
+    public AudioClip clip;
+}
+
+public class AudioManager : MonoBehaviour
+{
+    public MusicTrack[] tracks;
+    public AudioClip GetClipFromName(string trackName)
+    {
+        foreach (var track in tracks)
+        {
+            if (track.trackName == trackName)
+            {
+                return track.clip;
+            }
+        }
+
+        return null;
+    }
+}
